6c153805f8012bc1dd7543806dde9f74ebbba3ab
Laknonglak <donovanlak@gmail.com>
2023-08-20 12:30:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// calculate function
+function add (a,b){
+    console.log(a+b);
+}
+
+function subtract (a,b){
+    console.log(a-b);
+}
+
+function multiply (a,b){
+    console.log(a*b);
+}
+
+function divide (a,b){
+    console.log(a/b);
+}
+
+add(10,5);
+subtract(10,5);
+multiply(10,5);
+divide(10,5);
+
+//Another way with Return value
+function addTwoNumbers (num1,num2){
+    return (num1+num2);
+}
+
+function subtractTwoNumbers (num1,num2){
+    return(num1-num2);
+}
+
+function multiplyTwoNumbers (num1,num2){
+    return(num1*num2);
+}
+
+function divideTwoNumbers (num1,num2){
+    return(num1/num2);
+}
+
+console.log('result: ' + addTwoNumbers(10,5));
+console.log('result: ' + subtractTwoNumbers(10,5));
+console.log('result: ' + multiplyTwoNumbers(10,5));
+console.log('result: ' + divideTwoNumbers(10,5));
+
+
+//inform
+function informationCard(firstName,location,hobby){
+    console.log("Hi, my name is " + firstName + "I live in " + location + "and enjoy "+hobby);
+
+    //another way to get information
+    console.log(`Hi my name is "${firstName} I live in ${location} and enjoy ${hobby}`);
+}
+
+informationCard("Lak","Chiangrai","Cooking");
